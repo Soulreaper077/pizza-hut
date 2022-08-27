4b3a84779b6520294cb451b33b8a30bdc8b76399
Jesse <jesseponce233@gmail.com>
2022-08-26 20:44:41 -0500

    getting things done

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
 const {
   addComment,
-  removeComment
+  removeComment,
+  addReply,
+  removeReply, 
 } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
+// api for add reply 
+router
+.route('/:pizzaId/:commentId')
+.put(addReply)
+.delete(removeComment)
+
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
+
+//delete route fro remove reply 
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
